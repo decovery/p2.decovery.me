7c87b91137a91c88cb8b18d51611b3d315d6f1e0
decovery <aiste@akmila.lt>
2013-11-06 16:54:26 +0200

    unnecesary client files deleted

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -19,22 +19,6 @@ class index_controller extends base_controller {
 		
 		# Pass the signup module
 		$this->template->content->signup = View::instance('v_users_signup');
-	
-		# Load client files
-		# Head
-		$client_files_head = array(
-				'/css/profile.css',
-				'/js/profile.js'
-				);
-			
-		$this->template->client_files_head = Utils::load_client_files($client_files_head);
-		
-		# Body
-		$client_files_body = array(
-				'/js/profile.js'
-				);
-			
-		$this->template->client_files_body = Utils::load_client_files($client_files_body);
 						     		
 		# Render the view
 		echo $this->template;      					     		
